097ddf16e831ba4ec61dd77574c4bd4a9332c4ef
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-09-06 15:52:36 -0500

    registered models for admin

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from admin_app.models import InventoryItem, School, InventoryIntake, ScheduleItem, PencilBoxLocation, PencilBoxLocationInventoryItem
 # Register your models here.
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(InventoryIntake)
+class InventoryIntakeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ScheduleItem)
+class ScheduleItemAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PencilBoxLocation)
+class PencilBoxLocationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PencilBoxLocationInventoryItem)
+class PencilBoxLocationInventoryItemAdmin(admin.ModelAdmin):
+    pass
