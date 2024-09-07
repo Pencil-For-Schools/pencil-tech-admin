c0aaadbb6c8cb1d7092bbf6b51e7d103db157e82
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-09-07 10:17:16 -0500

    created superuser only admin class

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -2,30 +2,46 @@ from django.contrib import admin
 from admin_app.models import InventoryItem, School, InventoryIntake, ScheduleItem, PencilBoxLocation, PencilBoxLocationInventoryItem, IntakeType
 # Register your models here.
 
+class SuperuserOnlyAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        return request.user.is_superuser
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
 @admin.register(School)
-class SchoolAdmin(admin.ModelAdmin):
+class SchoolAdmin(SuperuserOnlyAdmin):
     pass
 
 @admin.register(InventoryItem)
-class InventoryItemAdmin(admin.ModelAdmin):
+class InventoryItemAdmin(SuperuserOnlyAdmin):
     pass
 
 @admin.register(InventoryIntake)
-class InventoryIntakeAdmin(admin.ModelAdmin):
+class InventoryIntakeAdmin(SuperuserOnlyAdmin):
     pass
 
 @admin.register(ScheduleItem)
-class ScheduleItemAdmin(admin.ModelAdmin):
+class ScheduleItemAdmin(SuperuserOnlyAdmin):
     pass
 
 @admin.register(PencilBoxLocation)
-class PencilBoxLocationAdmin(admin.ModelAdmin):
+class PencilBoxLocationAdmin(SuperuserOnlyAdmin):
     pass
 
 @admin.register(PencilBoxLocationInventoryItem)
-class PencilBoxLocationInventoryItemAdmin(admin.ModelAdmin):
+class PencilBoxLocationInventoryItemAdmin(SuperuserOnlyAdmin):
     pass
 
 @admin.register(IntakeType)
-class IntakeTypeAdmin(admin.ModelAdmin):
+class IntakeTypeAdmin(SuperuserOnlyAdmin):
     pass
---
+++ scheduling_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SchedulingAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scheduling_app'
