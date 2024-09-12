2d4dd895c99880a4c3095a6edebb23de101ee304
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-09-12 13:40:50 -0500

    add admin models and start shop flow

--- admin_app/admin/model_admin_views/__init__.py
+++ admin_app/admin/model_admin_views/__init__.py
@@ -7,3 +7,7 @@ from .pencil_box_location_inventory_item_admin import \
 from .schedule_item_admin import ScheduleItemAdmin  # noqa: F401
 from .school_admin import SchoolAdmin  # noqa: F401
 from .user_admin import UserAdmin  # noqa: F401
+from .teacher_admin import TeacherAdmin # noqa: F401
+from .county_admin import CountyAdmin # noqa: F401
+from .teacher_schedule_item_admin import TeacherScheduleItemAdmin # noqa: F401
+from .order_admin import OrderAdmin
