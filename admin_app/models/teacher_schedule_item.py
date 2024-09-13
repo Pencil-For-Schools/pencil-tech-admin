97459bf4377bea52dc643d202cf91718f6ec7c78
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-09-13 11:38:27 -0500

    add __str__ methods to models for admin

--- admin_app/models/teacher_schedule_item.py
+++ admin_app/models/teacher_schedule_item.py
@@ -1,4 +1,5 @@
 from django.db import models
+from admin_app.utils import format_date_time
 from .teacher import Teacher
 from .schedule_item import ScheduleItem
 from .order import Order
@@ -8,3 +9,7 @@ class TeacherScheduleItem(models.Model):
     teacher = models.ForeignKey(Teacher, on_delete=models.PROTECT)
     schedule_item = models.ForeignKey(ScheduleItem, on_delete=models.PROTECT)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        date, _ = format_date_time(self.schedule_item.date_time)
+        return f'{self.teacher.first_name} {self.teacher.last_name}, {date}'
