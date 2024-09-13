6f83b6405334dee571d2a379dd1a81016e6c3843
Justin Ferwerda <justin.ferwerda@gmail.com>
2024-09-13 09:42:42 -0500

    add associated inventory items to location

--- admin_app/admin/model_admin_views/pencil_box_location_admin.py
+++ admin_app/admin/model_admin_views/pencil_box_location_admin.py
@@ -1,7 +1,16 @@
 from unfold.admin import ModelAdmin
-
+from django.utils.html import format_html
+from django.urls import reverse
+from django.contrib import admin
 from admin_app.admin.configs import SuperuserOnlyAdminConfig
 
 
 class PencilBoxLocationAdmin(SuperuserOnlyAdminConfig, ModelAdmin):
-    pass
+    list_display = ('name', 'inventory_items_link')
+
+    def inventory_items_link(self, obj):
+        count = obj.inventory_items.count()
+        url = reverse('admin:admin_app_pencilboxlocationinventoryitem_changelist') + f'?pencil_box_location__id__exact={obj.id}'
+        return format_html('<a href="{}">{} Inventory items</a>', url, count)
+
+    inventory_items_link.short_description = 'Inventory Items'
