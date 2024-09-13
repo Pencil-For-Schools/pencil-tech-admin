b3b29a1a525059d49bbf6dd62cb959dacfb54ead
Trinity Christiana <trinitycterry@gmail.com>
2024-09-12 21:29:03 -0500

    flake 8 and packages

--- admin_app/apps.py
+++ admin_app/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 from django.contrib.auth.apps import AuthConfig
 
+
 class AdminAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_app'
+
 
 class CustomAuthConfig(AuthConfig):
     name = 'django.contrib.auth'
