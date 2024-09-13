5c0af671613fd6cd70f0ff8111f5fb78a3ccb5a9
JohnMDoll <johnmdoll3@gmail.com>
2024-09-13 14:29:13 -0500

    readonly on location items, location item update or create on intake

--- admin_app/admin/model_admin_views/inventory_intake_admin.py
+++ admin_app/admin/model_admin_views/inventory_intake_admin.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from unfold.decorators import display
 from admin_app.admin.configs import SuperuserOnlyAdminConfig
 from admin_app.models import PencilBoxLocationInventoryItem, IntakeType, InventoryItem
+from datetime import datetime
 
 
 class InventoryIntakeAdmin(SuperuserOnlyAdminConfig, ModelAdmin):
@@ -10,12 +11,20 @@ class InventoryIntakeAdmin(SuperuserOnlyAdminConfig, ModelAdmin):
     def save_model(self, request, obj, form, change):
         
         super().save_model(request, obj, form, change)
-        if obj.intake_type.name == "Add":
+        if obj.intake_type.name in ("Donation","Inventory Adjustment"):
             try:
                 location_item = PencilBoxLocationInventoryItem.objects.get(pencil_box_location=obj.pencil_box_location, inventory_item=obj.inventory_item)
-            except Exception:
-                PencilBoxLocationInventoryItem.create(
-                    
-                )
-            location_item.in_stock = obj.qty_donated
-            location_item.save()
+                location_item.in_stock += obj.qty_donated
+                location_item.save()
+            except PencilBoxLocationInventoryItem.DoesNotExist:
+                PencilBoxLocationInventoryItem.objects.create(
+                    inventory_item = obj.inventory_item,
+                    pencil_box_location = obj.pencil_box_location,
+                    bin_number = 0,
+                    low_stock = 0,
+                    max_amt = 0,
+                    sold = 0,
+                    in_stock = obj.qty_donated,
+                    last_audited = datetime.now(),
+                    archived = False
+                    )
--- admin_app/admin/model_admin_views/pencil_box_location_inventory_item_admin.py
+++ admin_app/admin/model_admin_views/pencil_box_location_inventory_item_admin.py
@@ -4,4 +4,4 @@ from admin_app.admin.configs import SuperuserOnlyAdminConfig
 
 
 class PencilBoxLocationInventoryItemAdmin(SuperuserOnlyAdminConfig, ModelAdmin):  # noqa: E501
-    pass
+    readonly_fields = ["sold","in_stock","last_audited"]
