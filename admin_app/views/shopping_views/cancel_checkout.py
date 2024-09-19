71ae46bd65f9b6502deb91dcf64ff576df86749a
Frank <frankcampos85@gmail.com>
2024-09-18 22:25:02 -0500

    Endpoints for cancel checkout and inventory_items_by_location (#59)
    * fc: I completed cancel_checkout and inventory_items_by_location endpoint
    
    * I tested the endpoint for cancel checkout with Postman, and it is working
    
    * fp: I tested the endpoint for get all items by location with Postman, it is working
    
    * fc: I completed cancel_checkout and inventory_items_by_location endpoint
    
    * I tested the endpoint for cancel checkout with Postman, and it is working
    
    * fp: I tested the endpoint for get all items by location with Postman, it is working
    
    * add email to cancel endpoint
    
    * Update Pipfile
    
    ---------
    
    Co-authored-by: Justin Ferwerda <justin.ferwerda@gmail.com>
    Co-authored-by: Trinity Christiana <trinitycterry@gmail.com>

---
+++ admin_app/views/shopping_views/cancel_checkout.py
@@ -0,0 +1,25 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from admin_app.models import TeacherScheduleItem
+from integrations.courier.resources.courier_messages.courier_message import CourierMessage
+
+class CancelCheckout(APIView):
+    def post(self, teacher_schedule_item_id):
+        try:
+            teacher_schedule_item = TeacherScheduleItem.objects.get(pk=teacher_schedule_item_id)
+            if teacher_schedule_item.order is None:
+                message_payload = {
+                    "name": teacher_schedule_item.teacher.first_name,
+                    "email": teacher_schedule_item.teacher.email
+                }
+                cancel_message = CourierMessage(
+                    'cancel_message_payload',
+                    payload_data=message_payload,
+                    message_type='cancel'
+                )
+                cancel_message.send_courier_message()
+                teacher_schedule_item.delete()
+                return Response({'message': 'RESERVATION_CANCELLED'})
+        except TeacherScheduleItem.DoesNotExist:
+            return Response({"message": "NO_SCHEDULED_SHOP"}, status=status.HTTP_400_BAD_REQUEST)
---
+++ admin_app/views/shopping_views/inventory_items_by_location.py
@@ -0,0 +1,28 @@
+# I need the teacher and pencil_box_location_inventory_item
+from admin_app.models import  PencilBoxLocationInventoryItem
+from rest_framework.response import Response
+from django.http import HttpResponseBadRequest
+from admin_app.serializers.inventory_app import PencilBoxLocationInventoryItemSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def inventory_items(request, location_id):
+    '''Get all the inventory items by location'''
+    try:
+
+        pencil_box_location_inventory_items = PencilBoxLocationInventoryItem.objects.filter(pencil_box_location_id=location_id).order_by('-bin_number')
+
+        if not pencil_box_location_inventory_items.exists():
+            return Response({"detail": "No items found at this location."}, status=status.HTTP_404_NOT_FOUND)
+
+        in_stock_items = pencil_box_location_inventory_items.filter(in_stock__gt=0)
+
+        serializer = PencilBoxLocationInventoryItemSerializer(in_stock_items, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    except PencilBoxLocationInventoryItem.DoesNotExist:
+        return HttpResponseBadRequest("I could not find any items at the specified location.")
+
+    except Exception as e:
+        return HttpResponseBadRequest(f"An error occurred: {str(e)}")
