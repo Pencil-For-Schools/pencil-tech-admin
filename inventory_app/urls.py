bba65c04ced51f75560ba115233a3a2820cccb3a
Trinity Christiana <trinitycterry@gmail.com>
2024-09-18 22:21:27 -0500

    updated order

--- inventory_app/urls.py
+++ inventory_app/urls.py
@@ -24,13 +24,11 @@ APP_NAME = 'api'
 
 urlpatterns = [
     path('admin/', admin_site.urls),
-    path(f'{APP_NAME}/shopping/start/<int:teacher_schedule_item_id>', StartShop.as_view(), name='start-shopping'),
-    path(f'{APP_NAME}/schedules', ScheduleList.as_view()),
+    path(f'{APP_NAME}/schools', SchoolList.as_view(),name='schools'),
+    path(f'{APP_NAME}/schedules', ScheduleList.as_view(), name='available_schedules'),
     path(f'{APP_NAME}/schedules/<int:pk>', ScheduleRetrieve.as_view(), name='schedule_by_id'),
-    path(f'{APP_NAME}/schools', SchoolList.as_view(),name='schools', name='available_schedules'),
     path(f'{APP_NAME}/schedules/<int:schedule_item_id>/slot_register', RegisterTeacherForScheduleItem.as_view(), name='slot_register'),
     path(f'{APP_NAME}/schedules/<int:schedule_item_id>/new_teacher', RegisterNewTeacherForScheduleItem.as_view(), name='new-teacher'),
-    path(f'{APP_NAME}/shopping/checkout', TeacherCheckout.as_view(), name='checkout')
-    # path(f'{APP_NAME}/shopping/cancel/<int:teacher_schedule_item_id>', .as_view(), name='cancel_reservation')
-    # path(f'{APP_NAME}/inventory_items/<int:location_id>', .as_view(), name='inventory_items')
+    path(f'{APP_NAME}/shopping/start/<int:teacher_schedule_item_id>', StartShop.as_view(), name='start-shopping'),
+    path(f'{APP_NAME}/shopping/checkout', TeacherCheckout.as_view(), name='checkout'),
 ]
